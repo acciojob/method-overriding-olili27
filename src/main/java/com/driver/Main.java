fd548b892cbc8aedffd8013094908035b6f00cb3
olili27 <timmolili27@gmail.com>
2023-03-19 23:03:31 +0300

    submit method overriding assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+   static class B extends A {
+
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.meth();
+
+        B obj2 = new B();
+        obj2.meth();
+    }
 }
