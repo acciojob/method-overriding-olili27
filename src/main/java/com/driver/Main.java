1029e0fa7577c742f9c9581383ec7478699cf360
olili27 <timmolili27@gmail.com>
2023-03-20 01:22:41 +0300

    fix bug 6

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,8 +17,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        A obj = new B();
-        obj.meth();
+        B obj = new B();
+        obj.meth(true);
 
         B obj2 = new B();
         obj2.meth();
