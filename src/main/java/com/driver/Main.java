6f7f722905c9c36025ce2529181ecfea54984dff
olili27 <timmolili27@gmail.com>
2023-03-19 23:21:44 +0300

    fix bug 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,15 +8,18 @@ public class Main {
     }
 
    static public class B extends A {
-
+        public void meth2() {
+            super.meth();
+        }
+        @Override
         public String meth() {
             return "Method is overridden in Extended class B";
         }
     }
 
     public static void main(String[] args) {
-        A obj = new B();
-        obj.meth();
+        B obj = new B();
+        obj.meth2();
 
         B obj2 = new B();
         obj2.meth();
